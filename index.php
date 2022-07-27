488f82aacaa5fa5e2a22ea46b93a3be971d5df28
Vetle Home <vetlestrandtun@gmail.com>
2022-07-27 14:39:52 +0200

    plant boxes is in the making

--- index.php
+++ index.php
@@ -2,16 +2,20 @@
 <html lang="no">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--<meta http-equiv="X-UA-Compatible" content="IE=edge">-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/phone.css">
+    <link rel="stylesheet" href="/myPlants/css/main.css" type="text/css">
+    <!-- <link rel="stylesheet" href="css/phone.css"> -->
     <title>My Plants</title>
 </head>
 <body>
     <header>
         <h1>myPlants</h1>
-        <div><div>Home</div><div>Plants</div><div>Calender</div></div>
+        <div class="head-container">
+            <div>Home</div>
+            <div>Plants</div>
+            <div>Calender</div>
+        </div>
     </header>
 
     <?php 
@@ -37,29 +41,34 @@
     <!-- Web site  -->
 
     <main>
-        <div>
+        <div class="plant_container">
         <?php 
         $sql = 'SELECT * FROM plant';
         $result = mysqli_query($conn, $sql);
         $rad = mysqli_fetch_assoc($result);
         
         while ($rad) {
+            // Get values
             $name = $rad['name'];
+            $image = $rad['picture'];
+
             // Create plant boxes
-            echo "  <div>
+            echo "  <div class='plant_box'>
                         <div>
-                            <img src='images/test.jpeg' alt='Tradescandia'>
+                            <img src='images/$image.jpg' alt='$name'>
                         </div>
                         <div>
                             <h2>$name</h2>
+                            <p>Vanning: 07/08/22</p>
                         </div>
                     </div>";
             
+            // Update $rad
             $rad = mysqli_fetch_assoc($result);
         }
         ?>
         </div>
-        <div>
+        <div class="plant_container">
             <!-- Next box -->
         </div>
     </main>
